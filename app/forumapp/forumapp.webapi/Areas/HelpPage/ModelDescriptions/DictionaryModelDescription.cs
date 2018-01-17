7f76672a2548f0735481bf2f1ef5fab132c775ad
Thiago Muramatsu <san.yusuke@gmail.com>
2018-01-17 21:59:59 -0200

    first

---
+++ app/forumapp/forumapp.webapi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace forumapp.webapi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ app/forumapp/forumapp.webapi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace forumapp.webapi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
